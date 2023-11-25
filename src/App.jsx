6bd4eb815afbaa0ab45e7e16ffde1807260f53b0
Julien Roche <merlin441977@gmail.com>
2023-11-25 13:37:24 +0100

    React formulaire contrôlé

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
 
-import Collapse from './components/collapse'
+
 import './App.css'
-import phare from './petr-slovacek-cW-FksiyWuo-unsplash.jpg'
+
 import { useState } from 'react'
 
-
+//input contrôlé par REACT, car l'input appelle la fonction handleChange à chaque entrée saisie dans l'input
 function App() {
-
-
-  //useState sur un objet :
-  const [person, setPerson] = useState({
-    firstName: "Jhon",
-    lastName: "Doe",
-    age: 18
-  })
-
-  const incrementerPerson = () => {
-    setPerson({ ...person, age: person.age + 1 })
+  const [firstName, setFirstName] = useState("Jhon")
+  const handleChange = (e) => {
+    setFirstName(e.currentTarget.value)
   }
-  //useState sur un tableau :
-  const [fruit, setFruit] = useState(["Banane", "Pomme", "Poire"])
-  const incrementerFruit = () => {
-    setFruit([...fruit, "Apple"])
-    console.log(fruit)
-  }
-  //useState simple changement de valeur unique d'une variable :
-  const [count, setCount] = useState(0)
-  const incrementerCounter = () => {
-    setCount(count + 1)
-  }
+
   return (
     <div>
+      <form>
+        <input type="text" name="firstName" onChange={handleChange} />{firstName}
 
-      <h1>Age de {person.firstName} : {person.age} Fruit : {fruit} counter : {count}</h1>
-      <button onClick={incrementerPerson}>Incrémenter age</button>
-      <button onClick={incrementerFruit}>Incrémenter fruit</button>
-      <button onClick={incrementerCounter}>Incrémenter counter</button>
+      </form>
 
     </div >
   );
